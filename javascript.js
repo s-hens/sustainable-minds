2efb33156663080085c901f2599f47d82a9c399a
s-hens <108551327+s-hens@users.noreply.github.com>
2023-06-26 12:24:50 +0100

    Test video performance with 3 files instead of 1

--- javascript.js
+++ javascript.js
@@ -2,11 +2,44 @@ let splashText = document.getElementById("splash-text");
 
 function splashCarousel() {
     if (splashText.textContent == "inspiring") {
+
         splashText.textContent = "empowering";
+
+        document.getElementById("splash-video2").style.display = "block";
+        document.getElementById("splash-video2").currentTime = 0;
+        document.getElementById("splash-video2").play();
+
+        document.getElementById("splash-video1").style.display = "none";
+        document.getElementById("splash-video3").style.display = "none";
+        document.getElementById("splash-video1").pause();
+        document.getElementById("splash-video3").pause();
+
     } else if (splashText.textContent == "empowering") {
-        splashText.textContent = "conscientious";
+
+        splashText.textContent = "actionable";
+
+        document.getElementById("splash-video3").style.display = "block";
+        document.getElementById("splash-video3").currentTime = 0;
+        document.getElementById("splash-video3").play();
+
+        document.getElementById("splash-video1").style.display = "none";
+        document.getElementById("splash-video2").style.display = "none";
+        document.getElementById("splash-video1").pause();
+        document.getElementById("splash-video2").pause();
+
     } else {
+
         splashText.textContent = "inspiring";
+
+        document.getElementById("splash-video1").style.display = "block";
+        document.getElementById("splash-video1").currentTime = 0;
+        document.getElementById("splash-video1").play();
+
+        document.getElementById("splash-video2").style.display = "none";
+        document.getElementById("splash-video3").style.display = "none";
+        document.getElementById("splash-video2").pause();
+        document.getElementById("splash-video3").pause();
+
     }
 }
 
