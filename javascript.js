54ff794d62c3f9df5b297b254cc0e7c5d42f714c
s-hens <108551327+s-hens@users.noreply.github.com>
2023-06-26 13:47:54 +0100

    Animate splash text

--- javascript.js
+++ javascript.js
@@ -1,46 +1,71 @@
-let splashText = document.getElementById("splash-text");
+const text1 = document.getElementById("splash-text1");
+const text2 = document.getElementById("splash-text2");
+const text3 = document.getElementById("splash-text3");
+
+const video1 = document.getElementById("splash-video1");
+const video2 = document.getElementById("splash-video2");
+const video3 = document.getElementById("splash-video3");
 
 function splashCarousel() {
-    if (splashText.textContent == "inspiring") {
+    if (text1.style.transform == "scale(1)") {
 
-        splashText.textContent = "empowering";
+        text2.style.transform = "scale(1)";
+        text2.style.opacity = "1";
 
-        document.getElementById("splash-video2").style.display = "block";
-        document.getElementById("splash-video2").currentTime = 0;
-        document.getElementById("splash-video2").play();
+        text1.style.transform = "scale(0)";
+        text3.style.transform = "scale(0)";
+        text1.style.opacity = "0";
+        text3.style.opacity = "0";
 
-        document.getElementById("splash-video1").style.display = "none";
-        document.getElementById("splash-video3").style.display = "none";
-        document.getElementById("splash-video1").pause();
-        document.getElementById("splash-video3").pause();
+        video2.style.display = "block";
+        video2.currentTime = 0;
+        video2.play();
 
-    } else if (splashText.textContent == "empowering") {
+        video1.style.display = "none";
+        video3.style.display = "none";
+        video1.pause();
+        video3.pause();
 
-        splashText.textContent = "actionable";
+    } else if (text2.style.transform == "scale(1)") {
 
-        document.getElementById("splash-video3").style.display = "block";
-        document.getElementById("splash-video3").currentTime = 0;
-        document.getElementById("splash-video3").play();
+        text3.style.transform = "scale(1)";
+        text3.style.opacity = "1";
 
-        document.getElementById("splash-video1").style.display = "none";
-        document.getElementById("splash-video2").style.display = "none";
-        document.getElementById("splash-video1").pause();
-        document.getElementById("splash-video2").pause();
+        text1.style.transform = "scale(0)";
+        text2.style.transform = "scale(0)";
+        text1.style.opacity = "0";
+        text2.style.opacity = "0";
+
+        video3.style.display = "block";
+        video3.currentTime = 0;
+        video3.play();
+
+        video1.style.display = "none";
+        video2.style.display = "none";
+        video1.pause();
+        video2.pause();
 
     } else {
 
-        splashText.textContent = "inspiring";
+        text1.style.transform = "scale(1)";
+        text1.style.opacity = "1";
 
-        document.getElementById("splash-video1").style.display = "block";
-        document.getElementById("splash-video1").currentTime = 0;
-        document.getElementById("splash-video1").play();
+        text2.style.transform = "scale(0)";
+        text3.style.transform = "scale(0)";
+        text2.style.opacity = "0";
+        text3.style.opacity = "0";
 
-        document.getElementById("splash-video2").style.display = "none";
-        document.getElementById("splash-video3").style.display = "none";
-        document.getElementById("splash-video2").pause();
-        document.getElementById("splash-video3").pause();
+        video1.style.display = "block";
+        video1.currentTime = 0;
+        video1.play();
+
+        video2.style.display = "none";
+        video3.style.display = "none";
+        video2.pause();
+        video3.pause();
 
     }
 }
 
+splashCarousel();
 setInterval(splashCarousel, 5000);
